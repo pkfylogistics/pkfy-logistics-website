98c3726c3265d3a287cd1b0abe9a77476844090c
pkfylogistics <pkfylogistics@gmail.com>
2026-07-14 13:30:33 -0400

    Add tracking function to handle shipment numbers

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function track(){
 
+let number=document.getElementById("tracking").value;
+
+
+if(number=="")
+{
+document.getElementById("result").innerHTML="Please enter tracking number";
+}
+
+else
+{
+document.getElementById("result").innerHTML=
+"Your shipment number "+number+" is being processed.";
+}
+
+}
